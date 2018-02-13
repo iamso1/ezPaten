dc939bc010599cae50e719010cbb8f75cb6be4bb
hsinhan <xxx@xxx.xx>
2018-02-13 17:09:23 +0800

    加入輸出邏輯
    1. 新增output class 實作輸出邏輯
    2. 主程式加入呼叫輸出邏輯

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import PatentCore from './patent-core';
+import OutPut from './output';
 
 (async () => {
+
   //create a patentCore instance 
   const patentCore = await PatentCore.create();
 
   //get IPCNums by PublicNums
   const arr_ipcNums = await patentCore.getIPCNumByPublicNum();
+  // console.log(arr_ipcNums);
 
   //get IPCNames by IPCNums
   const arr_ipcNames = await patentCore.getIPCNameByIPCNum(arr_ipcNums);
-  console.log(arr_ipcNames);
 
+  //write result to csv file
+  await OutPut.writeToCSV(arr_ipcNames);
+
+  await patentCore.close();
 })();
 
---
+++ src/output.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+
+export default class OutPut {
+    constructor() { }
+    public static async writeToCSV(arr_result: string[]) {
+        const test = await checkFile('./result/result.csv');
+        const writer = fs.createWriteStream('./result/result.csv', {
+            flags: 'a' // 'a' means appending (old data will be preserved)
+        });
+        arr_result.forEach((elm) => {
+            writer.write(elm + '\r');
+        });
+    }
+}
+
+//if file exist then remove the file
+function checkFile(filePath: string) {
+    return new Promise((resolve, reject) => {
+        fs.exists(filePath, (exists) => {
+            if (exists) {
+                fs.unlink(filePath, () => {
+                    resolve('in1')
+                });
+            } else {
+                resolve('in2');
+            }
+
+        });
+    });
+}
